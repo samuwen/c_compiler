ec0e8b20536bdf6cd38c37a5f2b8e5dc9a9b90f7
mark chaitin <mchaitin@gmail.com>
2020-11-03 22:18:03 -0500

    initial commit

---
+++ src/data/return_2.s
@@ -0,0 +1,12 @@
+	.file	"return_2.c"
+	.text
+	.section	.text.startup,"ax",@progbits
+	.p2align 4
+	.globl	main
+	.type	main, @function
+main:
+	movl	$2, %eax
+	ret
+	.size	main, .-main
+	.ident	"GCC: (GNU) 10.2.0"
+	.section	.note.GNU-stack,"",@progbits
