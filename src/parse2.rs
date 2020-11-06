b75847e7ac30083592401a618d1173d075d15b34
mark chaitin <mchaitin@gmail.com>
2020-11-06 16:16:21 -0500

    parser pretty print works

--- src/parse2.rs
+++ src/parse2.rs
@@ -30,7 +30,7 @@ impl Display for Prog {
 pub fn parse(mut tokens: Vec<Token>) -> Prog {
   let function = parse_function(&mut tokens);
   let json = to_string(&function._get_children()[0]._get_children()[0]).unwrap();
-  std::fs::write("out_json.json", &json).expect("Failed to write");
+  std::fs::write("out.json", &json).expect("Failed to write");
   debug!("{}", function);
   Prog::new(function)
 }
@@ -80,7 +80,7 @@ fn parse_statement(tokens: &mut Vec<Token>) -> Node<String> {
   if token.get_type() != &TokenType::ReturnKeyword {
     panic!("Statement does not have return keyword");
   }
-  n.add_data(String::from("return"));
+  n.add_data(String::from("return "));
   let expression = parse_expression(tokens);
   n.add_child(expression);
   let token = tokens.remove(0);
@@ -95,19 +95,20 @@ fn parse_statement(tokens: &mut Vec<Token>) -> Node<String> {
 fn parse_expression(tokens: &mut Vec<Token>) -> Node<String> {
   let mut n = Node::new("Expression");
   let error_message = "Unexpected termination of tokens";
-  let term = parse_term(tokens);
-  n.add_child(term);
+  let mut term = parse_term(tokens);
   let next = tokens.get(0).expect(error_message);
   let mut next_type = next.get_type().clone();
   while next_type == TokenType::Addition || next_type == TokenType::Negation {
     let op = tokens.remove(0);
-    let mut op_node = Node::new("Operator");
-    op_node.add_data(op.get_value());
-    n.add_child(op_node);
     let next_term = parse_term(tokens);
-    n.add_child(next_term);
+    let mut bin_op = Node::new("Binary Op");
+    bin_op.add_data(op.get_value());
+    bin_op.add_child(term);
+    bin_op.add_child(next_term);
+    term = bin_op;
     next_type = tokens.get(0).expect(error_message).get_type().clone();
   }
+  n.add_child(term);
   n
 }
 
@@ -115,19 +116,20 @@ fn parse_expression(tokens: &mut Vec<Token>) -> Node<String> {
 fn parse_term(tokens: &mut Vec<Token>) -> Node<String> {
   let mut n = Node::new("Term");
   let error_message = "Unexpected termination of tokens";
-  let factor = parse_factor(tokens);
-  n.add_child(factor);
+  let mut factor = parse_factor(tokens);
   let next = tokens.get(0).expect(error_message);
   let mut next_type = next.get_type().clone();
   while next_type == TokenType::Multiplication || next_type == TokenType::Division {
     let op = tokens.remove(0);
-    let mut op_node = Node::new("Operator");
-    op_node.add_data(op.get_value());
-    n.add_child(op_node);
     let next_factor = parse_factor(tokens);
-    n.add_child(next_factor);
+    let mut bin_op = Node::new("Binary Op");
+    bin_op.add_data(op.get_value());
+    bin_op.add_child(factor);
+    bin_op.add_child(next_factor);
+    factor = bin_op;
     next_type = tokens.get(0).expect(error_message).get_type().clone();
   }
+  n.add_child(factor);
   n
 }
 
@@ -137,12 +139,14 @@ fn parse_factor(tokens: &mut Vec<Token>) -> Node<String> {
   let token = tokens.remove(0);
   match token.get_type() {
     TokenType::OParen => {
-      let exp = parse_expression(tokens);
-      n.add_child(exp);
+      let mut exp = parse_expression(tokens);
+      exp.add_data(token.get_type().to_string());
       let token = tokens.remove(0);
       if token.get_type() != &TokenType::CParen {
         panic!("Open paren does not have matching close paren");
       }
+      exp.add_data(token.get_type().to_string());
+      n.add_child(exp);
     }
     TokenType::Integer => {
       let mut int_node = Node::new("Integer");
@@ -152,9 +156,9 @@ fn parse_factor(tokens: &mut Vec<Token>) -> Node<String> {
     TokenType::BitwiseComplement | TokenType::Negation | TokenType::LogicalNegation => {
       let mut op_node = Node::new("Unary Op");
       op_node.add_data(token.get_type().to_string());
-      n.add_child(op_node);
       let factor = parse_factor(tokens);
-      n.add_child(factor);
+      op_node.add_child(factor);
+      n.add_child(op_node);
     }
     _ => panic!("the disco"),
   }
@@ -226,54 +230,87 @@ impl Node<String> {
 
   fn add_expression_log(&self) -> String {
     let mut out_string = String::new();
-    let mut add_str = |s| {
-      out_string.push_str(" ");
-      out_string.push_str(s);
-    };
+    let mut result = vec![];
+    let mut has_parens = false;
     if self.data.len() > 0 {
-      for d in self.data.iter() {
-        add_str(&d);
-      }
+      has_parens = true;
     }
-    let list = self.get_data_nodes(0, vec![]);
-    debug!("{:?}", list);
-    let mut int_iter = list.iter().filter(|t| t.get_type() != "Operator");
-    let mut op_iter = list.iter().filter(|t| t.get_type() != "Integer");
-    for i in 0..list.len() {
-      let cd = match i % 2 == 0 {
-        true => int_iter.next().unwrap(),
-        false => op_iter.next().unwrap(),
+    for child in self.children.iter() {
+      let f = match child.get_type().as_str() {
+        "Binary Op" => Node::add_binary_op_log,
+        "Unary Op" => Node::add_unary_op_log,
+        "Term" => Node::add_term_log,
+        _ => panic!("Invalid child type in expression: {}", child.get_type()),
       };
-      cd.get_data().iter().for_each(|d| add_str(d));
+      result.push(f(child));
+    }
+    if has_parens {
+      result.insert(0, self.data.get(0).unwrap().to_owned());
+      result.push(self.data.last().unwrap().to_owned());
+    }
+    out_string.push_str(&result.join(""));
+    out_string
+  }
+
+  fn add_term_log(&self) -> String {
+    let mut out_string = String::new();
+    for child in self.children.iter() {
+      let f = match child.get_type().as_str() {
+        "Binary Op" => Node::add_binary_op_log,
+        "Factor" => Node::add_factor_log,
+        _ => panic!("Invalid child type in Term: {}", child.get_type()),
+      };
+      out_string.push_str(&f(child));
     }
     out_string
   }
 
-  fn get_data_nodes(&self, level: usize, mut list: Vec<ChildData>) -> Vec<ChildData> {
+  fn add_binary_op_log(&self) -> String {
+    let mut out_string = String::new();
+    let mut result: Vec<String> = vec![];
     for child in self.children.iter() {
-      match child.child_count > 0 {
-        true => {
-          list = child.get_data_nodes(level + 1, list);
-        }
-        false => {
-          let child_data = ChildData::new(level, child.get_data(), child.get_type());
-          let first_ele_pos = list
-            .iter()
-            .position(|item| item.get_level() < level && item.get_type() == child_data.get_type());
-          match first_ele_pos {
-            Some(val) => {
-              let ele = list.get(val).unwrap();
-              match ele.get_level() < level {
-                true => list.insert(val, child_data),
-                false => list.push(child_data),
-              }
-            }
-            None => list.push(child_data),
-          }
-        }
-      }
+      let f = match child.get_type().as_str() {
+        "Term" => Node::add_term_log,
+        "Factor" => Node::add_factor_log,
+        "Binary Op" => Node::add_binary_op_log,
+        _ => panic!("Invalid child type in binary op: {}", child.get_type()),
+      };
+      result.push(f(child));
     }
-    list
+    result.insert(1, self.data.get(0).expect("Binary op has no op").to_owned());
+    out_string.push_str(&result.join(" "));
+    out_string
+  }
+
+  fn add_factor_log(&self) -> String {
+    let mut out_string = String::new();
+    for child in self.children.iter() {
+      let f = match child.get_type().as_str() {
+        "Expression" => Node::add_expression_log,
+        "Unary Op" => Node::add_unary_op_log,
+        "Integer" => Node::add_integer_log,
+        _ => panic!("Invalid child type in factor: {}", child.get_type()),
+      };
+      out_string.push_str(&f(child));
+    }
+    out_string
+  }
+
+  fn add_unary_op_log(&self) -> String {
+    let mut out_string = String::new();
+    let mut result: Vec<String> = vec![
+      String::from(" "),
+      self.data.get(0).expect("Unary op has no op").to_owned(),
+    ];
+    for child in self.children.iter() {
+      result.push(child.add_factor_log());
+    }
+    out_string.push_str(&result.join(""));
+    out_string
+  }
+
+  fn add_integer_log(&self) -> String {
+    String::from(&self.data.join(" "))
   }
 }
 
@@ -285,46 +322,5 @@ impl Display for Node<String> {
       out_string.push_str(&child.add_statement_log());
     }
     write!(f, "\n{}", out_string)
-  }
-}
-
-#[derive(Clone, Debug, Eq, PartialEq)]
-struct ChildData {
-  data: Vec<String>,
-  level_count: usize,
-  _type: String,
-}
-
-impl ChildData {
-  fn new(count: usize, data: &Vec<String>, _type: &String) -> ChildData {
-    ChildData {
-      level_count: count,
-      data: data.to_owned(),
-      _type: _type.to_owned(),
-    }
-  }
-
-  fn get_data(&self) -> &Vec<String> {
-    &self.data
-  }
-
-  fn get_level(&self) -> usize {
-    self.level_count
-  }
-
-  fn get_type(&self) -> &String {
-    &self._type
-  }
-}
-
-impl Ord for ChildData {
-  fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-    self.level_count.cmp(&other.level_count)
-  }
-}
-
-impl PartialOrd for ChildData {
-  fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-    Some(self.cmp(other))
   }
 }
